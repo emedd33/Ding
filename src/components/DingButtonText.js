ad2bac1383d2cbd301deb2dc43553cd2b27350a5
Eskild Mageli <eskild.mageli@visma.com>
2020-10-17 20:00:38 +0200

    fix bug

--- src/components/DingButtonText.js
+++ src/components/DingButtonText.js
@@ -4,20 +4,20 @@ import Constants from 'expo-constants';
 import * as Font from 'expo-font';
 
 export default function DingButtonText() {
-  const [loaded] = Font.useFonts({
-    OldLodon: require('../assets/OldLondon.ttf'),
-  });
+  // const [loaded] = Font.useFonts({
+  //   OldLodon: require('../assets/OldLondon.ttf'),
+  // });
 
-  if (!loaded) {
-    return null;
-  }
+  // if (!loaded) {
+  //   return null;
+  // }
 
   return <Text style={styles.text} >Ding</Text>;
 }
 
 const styles = StyleSheet.create({
   text: {
-    fontFamily: "OldLondon",
+    // fontFamily: "OldLondon",
     textAlign:"center",
     fontSize:40
   }
